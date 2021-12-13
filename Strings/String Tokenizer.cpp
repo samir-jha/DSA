2861fd62ad6b425f9cbd54a629014dc5b2f153cf
Samir Jha <samirjha01101@gmail.com>
2021-12-13 09:35:45 +0530

    Added String Tokenizer.cpp

---
+++ Strings/String Tokenizer.cpp
@@ -0,0 +1,51 @@
+/*
+    Make your own tokenizer function that behaves identical to strtok() function.
+*/
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+char *mystrtok(char *str, char delimeter)
+{
+    static char *input = NULL;
+    if (str != NULL)
+    {
+        input = str;
+    }
+    if (input == NULL)
+    {
+        return NULL;
+    }
+    char *token = new char[strlen(input) + 1];
+    int i = 0;
+    for (; input[i] != '\0'; i++)
+    {
+        if (input[i] != delimeter)
+        {
+            token[i] = input[i];
+        }
+        else
+        {
+            token[i] = '\0';
+            input = input + i + 1;
+            return token;
+        }
+    }
+    token[i] = '\0';
+    input = NULL;
+    return token;
+}
+
+int main()
+{
+    char str[1000];
+    cin.getline(str, 1000);
+    char *token = mystrtok(str, ' ');
+    while (token != NULL)
+    {
+        cout << token << endl;
+        token = mystrtok(NULL, ' ');
+    }
+
+    return 0;
+}
